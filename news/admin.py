1e6c2b2d543707125b710b848d0885cd693ab905
equestriann <x.dormostuk@gmail.com>
2023-10-16 22:09:18 +0300

    all posts output ready

--- news/admin.py
+++ news/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Author, Category
 
 
 admin.site.register(Post)
-# admin.site.register(Product)
+admin.site.register(Author)
+admin.site.register(Category)
--- news/views.py
+++ news/views.py
@@ -2,6 +2,7 @@
 # что в этом представлении мы будем выводить список объектов из БД
 from django.views.generic import ListView, DetailView
 from .models import Post
+from datetime import datetime
 
 class PostList(ListView):
     # Указываем модель, объекты которой мы будем выводить
@@ -15,6 +16,21 @@ class PostList(ListView):
     # Его надо указать, чтобы обратиться к списку объектов в html-шаблоне.
     context_object_name = 'news_all'
 
+    # Метод get_context_data позволяет нам изменить набор данных,
+    # который будет передан в шаблон.
+    def get_context_data(self, **kwargs):
+        # С помощью super() мы обращаемся к родительским классам
+        # и вызываем у них метод get_context_data с теми же аргументами,
+        # что и были переданы нам.
+        # В ответе мы должны получить словарь.
+        context = super().get_context_data(**kwargs)
+        # К словарю добавим текущую дату в ключ 'time_now'.
+        context['time_now'] = datetime.utcnow()
+        # Добавим ещё одну пустую переменную,
+        # чтобы на её примере рассмотреть работу ещё одного фильтра.
+        context['news_quantity'] = None
+        return context
+
 class PostDetail(DetailView):
     # Модель всё та же, но мы хотим получать информацию по отдельному товару
     model = Post
@@ -22,3 +38,7 @@ class PostDetail(DetailView):
     template_name = 'news_one.html'
     # Название объекта, в котором будет выбранный пользователем продукт
     context_object_name = 'news'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['time'] = datetime.utcnow()
